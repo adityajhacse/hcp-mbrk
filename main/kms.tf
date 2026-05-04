0403b8322cc042981dea0f66dd450b0cefa48054
adityajhacse <54318636+adityajhacse@users.noreply.github.com>
2026-05-04 21:27:03 +0530

    Update kms.tf

--- main/kms.tf
+++ main/kms.tf
@@ -4,7 +4,7 @@ resource "aws_kms_key" "example" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-       
+     {  
         Effect = "Allow"
         Principal = {
           AWS = "*"
