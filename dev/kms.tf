04106b15a2f69fc15e732a8787e5223845a3779f
adityajhacse <54318636+adityajhacse@users.noreply.github.com>
2026-05-06 10:25:39 +0530

    Create kms.tf

---
+++ dev/kms.tf
@@ -0,0 +1,23 @@
+resource "aws_kms_key" "example" {
+  description = "KMS key for encrypting dev workspace"
+  key_usage  = "ENCRYPT_DECRYPT"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+     {  
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+ 
+
+provider "aws" {
+  region = "us-west-2"
+}
