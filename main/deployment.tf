68b05f6c957395d37830df565003730fcbe4c845
aditya jha <aditya.jha@salesforce.com>
2026-05-09 14:35:30 +0530

    apply ecs service

--- main/deployment.tf
+++ main/deployment.tf
@@ -152,9 +152,9 @@ resource "aws_security_group" "ecs" {
 
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/8"]
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["10.0.0.0/8"]
   }
 }
 
