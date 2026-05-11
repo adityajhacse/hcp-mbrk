eb5df259bd0762179ad48fc0ed414734bc89f79e
aditya jha <aditya.jha@salesforce.com>
2026-05-11 19:56:17 +0530

    apply ecs service

--- main/sg.tf
+++ main/sg.tf
@@ -5,9 +5,9 @@ resource "aws_security_group" "alb" {
 
   ingress {
     from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+  #  to_port     = 80
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
