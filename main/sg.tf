7c14a40a0067ffb40c4382dcf0c360b160d487e1
aditya jha <aditya.jha@salesforce.com>
2026-05-15 17:10:10 +0530

    test

--- main/sg.tf
+++ main/sg.tf
@@ -25,9 +25,9 @@ resource "aws_security_group" "ecs" {
 
   ingress {
     from_port       = 5678
-    to_port         = 5678
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
+  #to_port         = 5678
+   # protocol        = "tcp"
+    #security_groups = [aws_security_group.alb.id]
   }
 
   egress {
